6fbfff72e3c4ee592e3bf007c77ebc97b28bb598
itssweetrain <artraine@gmail.com>
2024-04-10 15:56:09 +0900

    add : AppDatabase 바인딩

--- app/src/main/java/com/example/android/hilt/di/DatabaseModule.kt
+++ app/src/main/java/com/example/android/hilt/di/DatabaseModule.kt
@@ -1,10 +1,14 @@
 package com.example.android.hilt.di
 
+import android.content.Context
+import androidx.room.Room
 import com.example.android.hilt.data.AppDatabase
 import com.example.android.hilt.data.LogDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
 
 /**
  * 모듈을 사용하여 Hilt에 바인딩 추가
@@ -13,6 +17,16 @@ import dagger.hilt.InstallIn
  */
 @Module
 object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext appContext: Context) : AppDatabase {
+        return Room.databaseBuilder(
+            appContext,
+            AppDatabase::class.java,
+            "logging.db"
+        ).build()
+    }
 
     /**
      * Hilt에 LogDao 인스턴스를 제공할때 database.logDao()가 실행되어야 한다고 알려줌
