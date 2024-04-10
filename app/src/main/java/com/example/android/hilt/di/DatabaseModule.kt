133141123b7f254d2a762fcb8d53293bf518b730
itssweetrain <artraine@gmail.com>
2024-04-10 15:52:58 +0900

    add : DatabaseModule 생성하여 Hilt에 바인딩

---
+++ app/src/main/java/com/example/android/hilt/di/DatabaseModule.kt
@@ -0,0 +1,24 @@
+package com.example.android.hilt.di
+
+import com.example.android.hilt.data.AppDatabase
+import com.example.android.hilt.data.LogDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+
+/**
+ * 모듈을 사용하여 Hilt에 바인딩 추가
+ * @Module 은 Hilt에 모듈임을 알려줌
+ * @InstallIn 은 어느 컨테이너에서 Hilt 구성요소를 지정하여 결합을 사용할 수 있는지 Hilt에 알려줌
+ */
+@Module
+object DatabaseModule {
+
+    /**
+     * Hilt에 LogDao 인스턴스를 제공할때 database.logDao()가 실행되어야 한다고 알려줌
+     */
+    @Provides
+    fun provideLogDao(database: AppDatabase) : LogDao {
+        return database.logDao()
+    }
+}
